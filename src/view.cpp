534e13c3594553a7526a37747e126f7d88da8de4
Joel de Guzman <djowel@gmail.com>
2016-08-20 23:52:06 +0800

    better round_rect implementation

--- src/view.cpp
+++ src/view.cpp
@@ -17,9 +17,13 @@ namespace photon
 
          c.fill_color(colors::blue.opacity(0.7));
          c.fill_round_rect(rect{ 10, 10, size{ 200, 200 } }, 40);
+         c.line_width(3);
+         c.stroke_round_rect(rect{ 10, 10, size{ 200, 200 } }, 40);
 
          c.fill_color(colors::red.opacity(0.7));
          c.fill_rect(rect{ 100, 100, size{ 200, 200 } });
+         c.line_width(3);
+         c.stroke_rect(rect{ 100, 100, size{ 200, 200 } });
       }
    );
 

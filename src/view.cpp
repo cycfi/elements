922b13a1fbe3c2b501fdab8446dc831fde39612f
Joel de Guzman <djowel@gmail.com>
2016-08-23 20:35:38 +0800

    text drawing updates

--- src/view.cpp
+++ src/view.cpp
@@ -27,10 +27,10 @@ namespace photon
 
 
          c.line_width(0.2);
-         c.stroke_style(colors::black.opacity(0.8));
+         c.stroke_style(colors::black.opacity(1));
 
          c.move_to(point{ 10, 50 });
-         c.line_to(point{ 600, 50 });
+         c.line_to(point{ 400, 50 });
          c.stroke();
 
          c.font("Helvetica", 16, c.bold);
@@ -38,34 +38,34 @@ namespace photon
          c.move_to(point{ 10, 100 });
          c.line_to(point{ 10, 10 });
          c.stroke();
-         c.text_align(c.align_baseline);
+         c.text_align(c.baseline);
          c.fill_text(point{ 10, 50 }, "j-Baseline");
 
          c.move_to(point{ 90, 100 });
          c.line_to(point{ 90, 10 });
          c.stroke();
-         c.text_align(c.align_top);
+         c.text_align(c.top);
          c.fill_text(point{ 90, 50 }, "j-Top");
 
          c.move_to(point{ 170, 100 });
          c.line_to(point{ 170, 10 });
          c.stroke();
-         c.text_align(c.align_middle);
+         c.text_align(c.middle);
          c.fill_text(point{ 170, 50 }, "j-Middle");
 
-         c.move_to(point{ 250, 300 });
+         c.move_to(point{ 250, 250 });
          c.line_to(point{ 250, 10 });
          c.stroke();
-         c.text_align(c.align_bottom);
+         c.text_align(c.bottom);
          c.fill_text(point{ 250, 50 }, "j-Bottom");
 
-         c.text_align(c.align_left);
+         c.text_align(c.left);
          c.fill_text(point{ 250, 100 }, "j-Left");
 
-         c.text_align(c.align_center);
+         c.text_align(c.center);
          c.fill_text(point{ 250, 150 }, "j-Center");
 
-         c.text_align(c.align_right);
+         c.text_align(c.right);
          c.fill_text(point{ 250, 200 }, "j-Right");
 
       }

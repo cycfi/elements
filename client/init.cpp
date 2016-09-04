02de261eeb58aede99662e7901c7255cf071e5b7
Joel de Guzman <djowel@gmail.com>
2016-09-04 11:48:15 +0800

    Added text drawing with aligns

--- client/init.cpp
+++ client/init.cpp
@@ -54,35 +54,78 @@ namespace client
          cnv.line_width(0.5);
 
          {
-             cnv.fill_style(colors::dim_gray);
-
-             auto r = rect{ 5, 25, 160, 160 };
-             cnv.fill_rect(r);
-             cnv.stroke_rect(r);
+            cnv.fill_style(colors::dim_gray);
+            auto r = rect{ 5, 25, 160, 160 };
+            cnv.fill_rect(r);
+            cnv.stroke_rect(r);
          }
 
          {
-             auto r = rect{ 50, 5, 100, 80 };
-             auto grad = canvas::linear_gradient{ { 50, 5 }, { 50, 50 } };
-             grad.add_color_stop({0, colors::dark_turquoise });
-             grad.add_color_stop({1, colors::midnight_blue });
-             cnv.fill_style(grad);
-             cnv.fill_round_rect(r, 5);
-             cnv.stroke_round_rect(r, 5);
+            auto r = rect{ 50, 5, 100, 80 };
+            auto grad = canvas::linear_gradient{ { 50, 5 }, { 50, 50 } };
+            grad.add_color_stop({0, colors::dark_turquoise });
+            grad.add_color_stop({1, colors::midnight_blue });
+            cnv.fill_style(grad);
+            cnv.fill_round_rect(r, 5);
+            cnv.stroke_round_rect(r, 5);
          }
 
          {
-             auto r = rect{ 80, 30, 130, 110 };
-             auto cp = center_point(r);
-             auto grad = canvas::radial_gradient{ cp, 5, cp, 50 };
-             grad.add_color_stop({0, colors::light_cyan });
-             grad.add_color_stop({1, colors::gold.opacity(0.3) });
-             cnv.fill_style(grad);
-             cnv.fill_round_rect(r, 10);
-             cnv.stroke_round_rect(r, 10);
+            auto r = rect{ 80, 30, 130, 110 };
+            auto cp = center_point(r);
+            auto grad = canvas::radial_gradient{ cp, 5, cp, 50 };
+            grad.add_color_stop({0, colors::light_cyan });
+            grad.add_color_stop({1, colors::gold.opacity(0.3) });
+            cnv.fill_style(grad);
+            cnv.fill_round_rect(r, 10);
+            cnv.stroke_round_rect(r, 10);
          }
 
+         {
+            cnv.fill_style(colors::dim_gray);
+            cnv.font("Open Sans", 36, cnv.bold | cnv.italic);
+            cnv.fill_text({ 200, 30 }, "Hello, World!");
+            cnv.stroke_text({ 200, 80 }, "Hello, World!");
 
+            auto grad = canvas::linear_gradient{ { 200, 50 }, { 400, 50 } };
+            grad.add_color_stop({0, colors::dark_turquoise });
+            grad.add_color_stop({1, colors::midnight_blue });
+            cnv.fill_style(grad);
+            cnv.fill_text({ 200, 130 }, "Hello, World!");
+         }
+
+         auto texty = [&cnv](float x, float y, char const* text)
+         {
+            cnv.stroke_style(colors::dim_gray);
+            cnv.line_width(0.5);
+            cnv.move_to({ x-100, y });
+            cnv.line_to({ x+100, y });
+            cnv.move_to({ x, y-50 });
+            cnv.line_to({ x, y+50 });
+            cnv.stroke();
+            
+            cnv.fill_style(colors::dim_gray);
+            cnv.font("Open Sans", 24);
+            cnv.fill_text({ x, y }, text);
+         };
+         
+         cnv.text_align(cnv.left);
+         texty(50, 250, "Lefty");
+
+         cnv.text_align(cnv.right);
+         texty(200, 250, "Righty");
+
+         cnv.text_align(cnv.center);
+         texty(300, 250, "Centery");
+
+         cnv.text_align(cnv.left | cnv.top);
+         texty(400, 250, "Topy");
+         
+         cnv.text_align(cnv.left | cnv.middle);
+         texty(500, 250, "Middley");
+
+         cnv.text_align(cnv.left | cnv.bottom);
+         texty(600, 250, "Bottomy");
       }
    };
 
@@ -98,8 +141,8 @@ namespace client
 
    void  init(view& v)
    {
-      //v.content.elements.push_back(new_(drawings{}));
-      v.content.elements.push_back(new_(background{}));
-      v.content.elements.push_back(new_(vpups));
+      v.content.elements.push_back(new_(drawings{}));
+      //v.content.elements.push_back(new_(background{}));
+      //v.content.elements.push_back(new_(vpups));
    }
 }

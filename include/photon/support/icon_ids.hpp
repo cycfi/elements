7498396a1667a5d35868a03ca9dc7492121b751c
Joel de Guzman <djowel@gmail.com>
2016-10-25 17:38:13 +0800

    copyright tweaks to make the lonk clickable

--- include/photon/support/icon_ids.hpp
+++ include/photon/support/icon_ids.hpp
@@ -1,7 +1,7 @@
 /*=======================================================================================
    Copyright (c) 2016 Joel de Guzman
 
-   Distributed under the MIT License (https://opensource.org/licenses/MIT)
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
 =======================================================================================*/
 #if !defined(PHOTON_GUI_LIB_ICON_IDS_JUNE_4_2016)
 #define PHOTON_GUI_LIB_ICON_IDS_JUNE_4_2016
--- platform/osx/photon/include/CocoaUtils.hpp
+++ platform/osx/photon/include/CocoaUtils.hpp
@@ -1,7 +1,7 @@
 /*=======================================================================================
    Copyright (c) 2016 Joel de Guzman
 
-   Distributed under the MIT License (https://opensource.org/licenses/MIT)
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
 =======================================================================================*/
 #if !defined(PHOTON_COCOA_UTILS_OCTOBER_15_2016)
 #define PHOTON_COCOA_UTILS_OCTOBER_15_2016

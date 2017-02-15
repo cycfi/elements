562493b97a189bb1682d522d04e5124c6c272a0a
Joel de Guzman <djowel@gmail.com>
2017-02-15 11:04:22 +0800

    Rearranging files for a modular app project development

--- apps/image/platform/osx/AppDelegate.h
+++ apps/image/platform/osx/AppDelegate.h
@@ -4,8 +4,8 @@
    Licensed under a Creative Commons Attribution-ShareAlike 4.0 International.
    http://creativecommons.org/licenses/by-sa/4.0/
 =================================================================================================*/
-#if !defined(INFINITY_APP_DELEGATE_OCTOBER_15_2016)
-#define INFINITY_APP_DELEGATE_OCTOBER_15_2016
+#if !defined(APP_DELEGATE_OCTOBER_15_2016)
+#define APP_DELEGATE_OCTOBER_15_2016
 
 #import <Cocoa/Cocoa.h>
 

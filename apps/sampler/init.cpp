27537aff8d1721280709bd1b2862faae10f03594
Joel de Guzman <djowel@gmail.com>
2017-02-17 18:19:25 +0800

    Photon GUI sampler

---
+++ apps/sampler/init.cpp
@@ -0,0 +1,148 @@
+/*=======================================================================================
+   Copyright (c) 2016-2017 Joel de Guzman
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=======================================================================================*/
+#include <photon.hpp>
+
+namespace client
+{
+   using namespace photon;
+
+   // Main window background color
+   auto bkd_color = color{ 35, 35, 37, 255 };
+
+   struct background : widget
+   {
+      void draw(context const& ctx)
+      {
+         auto&  cnv = ctx.canvas;
+         cnv.fill_style(bkd_color);
+         cnv.fill_rect(ctx.bounds);
+      }
+   };
+
+   auto box = basic(
+      [](context const& ctx)
+      {
+         auto& c = ctx.canvas;
+
+         c.begin_path();
+         c.round_rect(ctx.bounds, 4);
+         c.fill_style(colors::gold.opacity(0.8));
+         c.fill();
+      }
+   );
+
+   auto make_vtile()
+   {
+      auto _box = top_margin(
+         { 20 },
+         hsize(150, box)
+      );
+
+      return margin(
+         { 20, 50, 20, 20 },
+         vtile(
+            halign(0.0, _box),
+            halign(0.2, _box),
+            halign(0.4, _box),
+            halign(0.6, _box),
+            halign(0.8, _box),
+            halign(1.0, _box)
+         )
+      );
+   }
+
+   auto make_vtile2()
+   {
+      auto _box = top_margin(
+         { 20 },
+         box
+      );
+
+      return margin(
+         { 20, 50, 20, 20 },
+         vtile(
+            vspan(1.0, _box),
+            vspan(0.5, _box),
+            vspan(0.5, _box),
+            vspan(0.5, _box),
+            vspan(2.0, _box)
+         )
+      );
+   }
+
+   auto make_vtile_main()
+   {
+      return htile(
+         margin({ 10, 10, 10, 10 }, group("Aligns", make_vtile(), 0.8, false)),
+         margin({ 10, 10, 10, 10 }, group("Percentages", make_vtile2(), 0.8, false))
+      );
+   }
+
+   auto make_htile()
+   {
+      auto _box = left_margin(
+         { 20 },
+         vsize(100, box)
+      );
+
+      return margin(
+         { 0, 50, 20, 20 },
+         htile(
+            valign(0.0, _box),
+            valign(0.2, _box),
+            valign(0.4, _box),
+            valign(0.6, _box),
+            valign(0.8, _box),
+            valign(1.0, _box)
+         )
+      );
+   }
+
+   auto make_htile2()
+   {
+      auto _box = left_margin(
+         { 20 },
+         box
+      );
+
+      return margin(
+         { 0, 50, 20, 20 },
+         htile(
+            hspan(1.0, _box),
+            hspan(0.5, _box),
+            hspan(0.5, _box),
+            hspan(0.5, _box),
+            hspan(2.0, _box)
+         )
+      );
+   }
+
+   auto make_htile_main()
+   {
+      return htile(
+         margin({ 10, 10, 10, 10 }, group("Aligns", make_htile(), 0.8, false)),
+         margin({ 10, 10, 10, 10 }, group("Percentages", make_htile2(), 0.8, false))
+      );
+   }
+
+   auto make_view_port()
+   {
+      auto  space = image{ "images/space.jpg" };
+      return scroller(space);
+   }
+
+   init_view _init_view
+   {
+      [](photon::view& view_)
+      {
+         view_.content =
+         {
+            share(background{}),
+            share(make_htile_main())
+         };
+      }
+   };
+}
---
+++ apps/sampler/platform/osx/AppDelegate.h
@@ -0,0 +1,15 @@
+/*=================================================================================================
+   Copyright (c) 2016 Joel de Guzman
+
+   Licensed under a Creative Commons Attribution-ShareAlike 4.0 International.
+   http://creativecommons.org/licenses/by-sa/4.0/
+=================================================================================================*/
+#if !defined(APP_DELEGATE_OCTOBER_15_2016)
+#define APP_DELEGATE_OCTOBER_15_2016
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+#endif

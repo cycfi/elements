553bba3b38ace2d05d41ad6f43eedfb99b3cb749
djowel <djowel@gmail.com>
2017-12-04 17:42:06 +0800

    Creating a basic window in linux

--- photon_lib/host/linux/app.cpp
+++ photon_lib/host/linux/app.cpp
@@ -4,6 +4,17 @@
    Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
 =============================================================================*/
 #include <photon/host.hpp>
+#include <gtk/gtk.h>
+
+namespace
+{
+   void activate(GtkApplication* app, gpointer user_data)
+   {
+      GtkWidget* window = gtk_application_window_new(app);
+      gtk_window_set_title(GTK_WINDOW(window), "Hello GNOME");
+      gtk_widget_show_all(window);
+   }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Application event loop entry
@@ -11,7 +22,16 @@ namespace photon
 {
    int app_main(int argc, const char* argv[])
    {
-      return 0;
+      GtkApplication* app = gtk_application_new(
+         "org.gtk.example", G_APPLICATION_FLAGS_NONE
+      );
+
+      g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
+      int status = g_application_run(
+         G_APPLICATION(app), argc, const_cast<char**>(argv)
+      );
+      g_object_unref(app);
+      return status;
    }
 }
 

935068b1d638d17cfb1506d5266c3262e1d3a51a
djowel <djowel@gmail.com>
2017-12-08 11:54:13 +0800

    Added direct to struct JSON support.

---
+++ photon_lib/include/photon/support/json.hpp
@@ -0,0 +1,245 @@
+/*=============================================================================
+   Copyright (c) 2016-2017 Joel de Guzman
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#if !defined(PHOTON_GUI_LIB_JSON_DECEMBER_5_2017)
+#define PHOTON_GUI_LIB_JSON_DECEMBER_5_2017
+
+#include <type_traits>
+#include <utility>
+#include <cctype>
+#include <vector>
+#include <string>
+#include <array>
+
+#include <boost/spirit/home/x3.hpp>
+#include <boost/fusion/include/is_sequence.hpp>
+#include <boost/regex/pending/unicode_iterator.hpp>
+
+#include <photon/support/exception.hpp>
+
+namespace photon { namespace json
+{
+   namespace x3 = boost::spirit::x3;
+
+   struct malformed_json_string : exception
+   {
+      explicit malformed_json_string(std::string const& what, char const* where)
+       : exception(what)
+       , where(where)
+      {}
+
+      char const* const where;
+   };
+
+   class string
+   {
+   public:
+
+      string()
+       : _first(nullptr), _last(nullptr)
+      {}
+
+      string(char const* first, char const* last)
+       : _first(first), _last(last)
+      {}
+
+      string(string const&) = default;
+      string(string&&) = default;
+
+      string& operator=(string const&) = default;
+      string& operator=(string&&) = default;
+
+      explicit operator std::string() const;
+
+      char const* begin() const { return _first; }
+      char const* end() const { return _last; }
+
+   private:
+
+      void error(std::string s, char const* where) const
+      {
+         throw malformed_json_string{"Error: Malformed JSON string. " + s, where};
+      }
+
+      char const* _first;
+      char const* _last;
+   };
+
+   // The JSON parser
+   struct parser : x3::parser<parser>
+   {
+      // Floating point numbers
+      template <typename Iter, typename Attribute>
+      typename std::enable_if<std::is_floating_point<Attribute>::value, bool>::type
+      parse(Iter& first, Iter last, Attribute& val) const;
+
+      // Integers
+      template <typename Iter, typename Attribute>
+      typename std::enable_if<std::is_integral<Attribute>::value, bool>::type
+      parse(Iter& first, Iter last, Attribute& val) const;
+
+      // Boolean
+      template <typename Iter>
+      bool parse(Iter& first, Iter last, bool& val) const;
+
+      // String
+      template <typename Iter>
+      bool parse(Iter& first, Iter last, string& val) const;
+
+      template <typename Iter>
+      bool parse(Iter& first, Iter last, std::string& val) const;
+
+      // Object
+      template <typename Iter, typename Attribute>
+      typename std::enable_if<
+         boost::fusion::traits::is_sequence<Attribute>::value, bool
+      >::type
+      parse(Iter& first, Iter last, Attribute& val) const;
+
+      // Array
+      template <typename Iter, typename Attribute>
+      bool parse(Iter& first, Iter last, std::vector<Attribute>& val) const;
+
+      template <typename Iter, typename Attribute, std::size_t N>
+      bool parse(Iter& first, Iter last, std::array<Attribute, N>& val) const;
+
+      // Main parse entry point
+      template <typename Iterator, typename Attribute>
+      bool parse(Iterator& first, Iterator const& last
+       , x3::unused_type, x3::unused_type, Attribute& attr) const
+      {
+         return parse(first, last, attr);
+      }
+   };
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Implementation
+   ////////////////////////////////////////////////////////////////////////////
+
+   string::operator std::string() const
+   {
+      using uchar = boost::uint32_t;   // a unicode code point
+      std::string result;              // result string is compiled here
+      char const* i = _first + 1;      // skip the initial '"'
+      char const* last = _last - 1;    // skip the final '"'
+
+      for (; i < last; ++i)
+      {
+         auto ch = *i;
+         if (std::iscntrl(ch))
+            error("Invalid control character", i);
+
+         if (*i == '\\')
+         {
+            if (++i == last)
+               error("Unexpected end of string", i);
+
+            switch (ch = *i)
+            {
+               case 'b': result += '\b';     break;
+               case 'f': result += '\f';     break;
+               case 'n': result += '\n';     break;
+               case 'r': result += '\r';     break;
+               case 't': result += '\t';     break;
+               default: result += ch;        break;
+
+               case 'u':
+               {
+                  x3::uint_parser<uchar, 16, 4, 4> hex4;
+                  uchar code_point;
+                  auto ii = ++i;
+                  bool r = x3::parse(ii, last, hex4, code_point);
+                  if (!r)
+                     error("Bad hexadecimal number: " + std::string(i, last), i);
+
+                  i = ii; // update iterator position
+                  using insert_iter = std::back_insert_iterator<std::string>;
+                  insert_iter out_iter(result);
+                  boost::utf8_output_iterator<insert_iter> utf8_iter(out_iter);
+                  *utf8_iter++ = code_point;
+               }
+            }
+         }
+         else
+         {
+            result += ch;
+         }
+      }
+      return result;
+   }
+
+   template <typename Iter, typename Attribute>
+   inline typename std::enable_if<std::is_floating_point<Attribute>::value, bool>::type
+   parser::parse(Iter& first, Iter last, Attribute& val) const
+   {
+      return x3::parse(first, last, x3::real_parser<Attribute>{}, val);
+   }
+
+   template <typename Iter, typename Attribute>
+   inline typename std::enable_if<std::is_integral<Attribute>::value, bool>::type
+   parser::parse(Iter& first, Iter last, Attribute& val) const
+   {
+      return x3::parse(first, last, x3::int_parser<Attribute>{}, val);
+   }
+
+   template <typename Iter>
+   inline bool
+   parser::parse(Iter& first, Iter last, bool& val) const
+   {
+      return x3::parse(first, last, x3::bool_, val);
+   }
+
+   template <typename Iter>
+   inline bool
+   parser::parse(Iter& first, Iter last, string& val) const
+   {
+      // Parse this manually for speed. We do not decode nor validate anything.
+      // We simply detect what looks like a double-quoted string which contains
+      // any character, including any escaped double quote (i.e. "\\\""). We'll
+      // deal with the actual string validation and extraction later.
+
+      static auto double_quote = '"' >> *("\\\"" | (x3::char_ - '"')) >> '"';
+      auto iter = first;
+      bool r = x3::parse(iter, last, double_quote);
+      if (r)
+         val = {first, iter};
+      return r;
+   }
+
+   template <typename Iter>
+   inline bool
+   parser::parse(Iter& first, Iter last, std::string& val) const
+   {
+      json::string s;
+      bool r = parse(first, last, s);
+      if (r)
+         val = std::string(s);
+      return r;
+   }
+
+   template <typename Iter, typename Attribute>
+   inline typename std::enable_if<boost::fusion::traits::is_sequence<Attribute>::value, bool>::type
+   parser::parse(Iter& first, Iter last, Attribute& val) const
+   {
+      return false;
+   }
+
+   template <typename Iter, typename Attribute>
+   inline bool
+   parser::parse(Iter& first, Iter last, std::vector<Attribute>& val) const
+   {
+      static auto g = '[' >> *(*this) >> ']';
+      return x3::parse(first, last, g, x3::space, val);
+   }
+
+   template <typename Iter, typename Attribute, std::size_t N>
+   inline bool
+   parser::parse(Iter& first, Iter last, std::array<Attribute, N>& val) const
+   {
+      return false;
+   }
+}}
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,75 @@
+/*=============================================================================
+   Copyright (c) 2016-2017 Joel de Guzman
+
+   Distributed under the MIT License (https://opensource.org/licenses/MIT)
+=============================================================================*/
+#include <boost/detail/lightweight_test.hpp>
+#include <photon/support/json.hpp>
+
+namespace json = photon::json;
+namespace x3 = boost::spirit::x3;
+
+template <typename T>
+void test_parser(json::parser const& jp, char const* in, T n)
+{
+   char const* f = in;
+   char const* l = f + std::strlen(f);
+
+   decltype(n) attr;
+   bool r = x3::parse(f, l, jp, attr);
+   BOOST_TEST(r);
+   BOOST_TEST(attr == n);
+};
+
+void test_string1(json::parser const& jp, char const* in)
+{
+   char const* f = in;
+   char const* l = f + std::strlen(f);
+
+   json::string s;
+   bool r = x3::parse(f, l, jp, s);
+   BOOST_TEST(r);
+   BOOST_TEST(s.begin() == in);
+   BOOST_TEST(s.end() == l);
+};
+
+void test_string2(json::parser const& jp, char const* in, std::string s)
+{
+   char const* f = in;
+   char const* l = f + std::strlen(f);
+
+   std::string attr;
+   bool r = x3::parse(f, l, jp, attr);
+   BOOST_TEST(r);
+   BOOST_TEST(attr == s);
+};
+
+void test_json()
+{
+   json::parser jp;
+
+   {
+      test_parser(jp, "1234", 1234);
+      test_parser(jp, "1234.45", 1234.45);
+      test_parser(jp, "true", true);
+      test_parser(jp, "false", false);
+   }
+
+   {
+      test_string1(jp, "\"This is my string\"");
+      test_string1(jp, "\"This is \\\"my\\\" string\"");
+   }
+
+   {
+      test_string2(jp, "\"This is my string\"", "This is my string");
+      test_string2(jp, "\"This is \\\"my\\\" string\"", "This is \"my\" string");
+      test_string2(jp, "\"Sosa did fine.\\u263A\"", u8"Sosa did fine.\u263A");
+      test_string2(jp, "\"Snowman: \\u2603\"", u8"Snowman: \u2603");
+   }
+}
+
+int main(int argc, const char* argv[])
+{
+   test_json();
+   return boost::report_errors();
+}

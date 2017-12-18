a6e88549dc41b20f9f30ab7cbd855d64eb02a30b
djowel <djowel@gmail.com>
2017-12-18 17:47:09 +0800

    Small steps. Linux port now has a JSON app config

--- photon_lib/host/linux/app.cpp
+++ photon_lib/host/linux/app.cpp
@@ -4,16 +4,47 @@
    Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
 =============================================================================*/
 #include <photon/host.hpp>
+#include <photon/support/load_json.hpp>
+#include <photon/support/assert.hpp>
 #include <gtk/gtk.h>
+#include <string>
 
-namespace
+namespace x3 = boost::spirit::x3;
+namespace json = photon::json;
+namespace fs = boost::filesystem;
+
+struct config
 {
-   void activate(GtkApplication* app, gpointer user_data)
-   {
-      GtkWidget* window = gtk_application_window_new(app);
-      gtk_window_set_title(GTK_WINDOW(window), "Hello GNOME");
-      gtk_widget_show_all(window);
-   }
+   std::string application_name;
+   std::string application_id;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(
+   config,
+   (std::string, application_name)
+   (std::string, application_id)
+)
+
+struct activate_info
+{
+   config& cfg;
+};
+
+config get_config()
+{
+   fs::path path = "config.json";
+   PHOTON_ASSERT(boost::filesystem::exists(path), "Error: config.json not exist.");
+   auto r = json::load<config>(path);
+   PHOTON_ASSERT(r, "Error: Invalid config.json.");
+   return r.get();
+}
+
+void activate(GtkApplication* app, gpointer user_data)
+{
+   auto pinfo = static_cast<activate_info const*>(user_data);
+   GtkWidget* window = gtk_application_window_new(app);
+   gtk_window_set_title(GTK_WINDOW(window), pinfo->cfg.application_name.c_str());
+   gtk_widget_show_all(window);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -22,11 +53,14 @@ namespace photon
 {
    int app_main(int argc, const char* argv[])
    {
+      config cfg = get_config();
+      activate_info info = { cfg };
+
       GtkApplication* app = gtk_application_new(
-         "org.gtk.example", G_APPLICATION_FLAGS_NONE
+         cfg.application_id.c_str(), G_APPLICATION_FLAGS_NONE
       );
 
-      g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
+      g_signal_connect(app, "activate", G_CALLBACK(activate), &info);
       int status = g_application_run(
          G_APPLICATION(app), argc, const_cast<char**>(argv)
       );
---
+++ photon_lib/include/photon/support/load_json.hpp
@@ -0,0 +1,36 @@
+/*=============================================================================
+   Copyright (c) 2016-2017 Joel de Guzman
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#if !defined(PHOTON_GUI_LIB_LOAD_JSON_DECEMBER_18_2017)
+#define PHOTON_GUI_LIB_LOAD_JSON_DECEMBER_18_2017
+
+#include <photon/support/json.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/optional.hpp>
+
+namespace photon { namespace json
+{
+   namespace fs = boost::filesystem;
+
+   template <typename T>
+   boost::optional<T> load(fs::path path)
+   {
+      fs::ifstream file(path);
+      std::string src(
+         (std::istreambuf_iterator<char>(file))
+       , std::istreambuf_iterator<char>());
+      T data;
+      auto f = src.begin();
+      auto l = src.end();
+      if (x3::phrase_parse(f, l, parser{}, x3::space, data))
+         return data;
+      else
+         return {};
+   }
+
+}}
+
+#endif

47d6522b13d691a53d73629c228599663d7a924a
djowel <djowel@gmail.com>
2018-09-17 20:26:13 +0800

    renema load_json to json_io, and added json::save

--- photon_lib/include/photon/support/json_io.hpp
+++ photon_lib/include/photon/support/json_io.hpp
@@ -18,19 +18,29 @@ namespace photon { namespace json
    template <typename T>
    boost::optional<T> load(fs::path path)
    {
-      fs::ifstream file(path);
-      std::string src(
-         (std::istreambuf_iterator<char>(file))
-       , std::istreambuf_iterator<char>());
-      T data;
-      auto f = src.begin();
-      auto l = src.end();
-      if (x3::phrase_parse(f, l, parser{}, x3::space, data))
-         return { std::move(data) };
-      else
-         return {};
+      if (fs::exists(path))
+      {
+         fs::ifstream file(path);
+         std::string src(
+            (std::istreambuf_iterator<char>(file))
+         , std::istreambuf_iterator<char>());
+         T data;
+         auto f = src.begin();
+         auto l = src.end();
+         if (x3::phrase_parse(f, l, parser{}, x3::space, data))
+            return { std::move(data) };
+      }
+      return {};
    }
 
+   template <typename T>
+   void save(fs::path path, T const& attr)
+   {
+      fs::ofstream file(path);
+      std::string json;
+      printer pr(file);
+      pr.print(attr);
+   }
 }}
 
 #endif

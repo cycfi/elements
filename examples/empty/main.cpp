3944efb12b87b21265c74b48e49c3bf6333daa4c
djowel <djowel@gmail.com>
2019-06-09 18:35:19 +0800

    Added empty starter app

---
+++ examples/empty/main.cpp
@@ -0,0 +1,25 @@
+#include <photon.hpp>
+
+using namespace cycfi::photon;
+
+int main(int argc, const char* argv[])
+{
+   app _app(argc, argv);
+   window _win(_app.name());
+   _win.on_close = [&_app]() { _app.stop(); };
+
+   view view_(_win);
+
+   view_.content(
+      {
+                           // Add more content layers here. The order
+                           // specifies the layering. The lowest layer
+                           // is at the top of this list.
+
+         share(element())  // Replace element() with your main element.
+      }
+   );
+
+   _app.run();
+   return 0;
+}

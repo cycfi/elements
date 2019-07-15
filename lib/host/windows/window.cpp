d402d120e9e601223e73b24ddbc2b3f973fe5a08
Joel de Guzman <djowel@gmail.com>
2019-07-15 18:39:46 +0800

    unnamed namespace

--- lib/host/windows/window.cpp
+++ lib/host/windows/window.cpp
@@ -10,63 +10,66 @@ namespace elements = cycfi::elements;
 
 namespace cycfi { namespace elements
 {
-   void DisableCloseButton(HWND hwnd)
+   namespace
    {
-      EnableMenuItem(GetSystemMenu(hwnd, FALSE), SC_CLOSE,
-         MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
-   }
-
-   void DisableMinimizeButton(HWND hwnd)
-   {
-      SetWindowLong(hwnd, GWL_STYLE,
-         GetWindowLong(hwnd, GWL_STYLE) & ~WS_MINIMIZEBOX);
-   }
-
-   void DisableMaximizeButton(HWND hwnd)
-   {
-      SetWindowLong(hwnd, GWL_STYLE,
-         GetWindowLong(hwnd, GWL_STYLE) & ~WS_MAXIMIZEBOX);
-   }
-
-   void DisableResizing(HWND hwnd)
-   {
-      SetWindowLong(hwnd, GWL_STYLE,
-         GetWindowLong(hwnd, GWL_STYLE) & ~WS_SIZEBOX);
-      DisableMaximizeButton(hwnd);
-   }
-
-   LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
-   {
-      auto param = GetWindowLongPtr(hwnd, GWLP_USERDATA);
-      auto* p = reinterpret_cast<window*>(param);
-      switch (message)
+      void DisableCloseButton(HWND hwnd)
       {
-         case WM_DESTROY:
-            if (p)
-               p->on_close();
-            break;
-
-         default:
-            return DefWindowProc(hwnd, message, wparam, lparam);
+         EnableMenuItem(GetSystemMenu(hwnd, FALSE), SC_CLOSE,
+            MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
       }
-      return 0;
-   }
 
-   struct init_window_class
-   {
-      init_window_class()
+      void DisableMinimizeButton(HWND hwnd)
       {
-         WNDCLASS windowClass = {0};
-         windowClass.hbrBackground = NULL;
-         windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-         windowClass.hInstance = NULL;
-         windowClass.lpfnWndProc = WndProc;
-         windowClass.lpszClassName = "ElementsWindow";
-         windowClass.style = CS_HREDRAW | CS_VREDRAW;
-         if (!RegisterClass(&windowClass))
-            MessageBox(nullptr, "Could not register class", "Error", MB_OK);
+         SetWindowLong(hwnd, GWL_STYLE,
+            GetWindowLong(hwnd, GWL_STYLE) & ~WS_MINIMIZEBOX);
       }
-   };
+
+      void DisableMaximizeButton(HWND hwnd)
+      {
+         SetWindowLong(hwnd, GWL_STYLE,
+            GetWindowLong(hwnd, GWL_STYLE) & ~WS_MAXIMIZEBOX);
+      }
+
+      void DisableResizing(HWND hwnd)
+      {
+         SetWindowLong(hwnd, GWL_STYLE,
+            GetWindowLong(hwnd, GWL_STYLE) & ~WS_SIZEBOX);
+         DisableMaximizeButton(hwnd);
+      }
+
+      LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+      {
+         auto param = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+         auto* p = reinterpret_cast<window*>(param);
+         switch (message)
+         {
+            case WM_DESTROY:
+               if (p)
+                  p->on_close();
+               break;
+
+            default:
+               return DefWindowProc(hwnd, message, wparam, lparam);
+         }
+         return 0;
+      }
+
+      struct init_window_class
+      {
+         init_window_class()
+         {
+            WNDCLASS windowClass = {0};
+            windowClass.hbrBackground = NULL;
+            windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+            windowClass.hInstance = NULL;
+            windowClass.lpfnWndProc = WndProc;
+            windowClass.lpszClassName = "ElementsWindow";
+            windowClass.style = CS_HREDRAW | CS_VREDRAW;
+            if (!RegisterClass(&windowClass))
+               MessageBox(nullptr, "Could not register class", "Error", MB_OK);
+         }
+      };
+   }
 
    window::window(std::string const& name, int style_, rect const& bounds)
    {

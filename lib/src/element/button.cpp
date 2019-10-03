8ddd11e44956b7a67427b5223ad9310d94c0c270
djowel <djowel@gmail.com>
2019-10-03 11:09:14 +0800

    call to value(val) should not call on_click

--- lib/src/element/button.cpp
+++ lib/src/element/button.cpp
@@ -59,11 +59,7 @@ namespace cycfi { namespace elements
    void basic_button::value(bool new_state)
    {
       if (_state != new_state)
-      {
          state(new_state);
-         if (on_click)
-            on_click(_state);
-      }
    }
 
    void basic_button::value(int new_state)
@@ -122,11 +118,7 @@ namespace cycfi { namespace elements
    void layered_button::value(bool new_state)
    {
       if (_state != new_state)
-      {
          state(new_state);
-         if (on_click)
-            on_click(_state);
-      }
    }
 
    void layered_button::value(int new_state)

9e73461a0520bd13c74969e35a3a5939dc3618d0
djowel <djowel@gmail.com>
2019-10-22 22:30:52 +0800

    adjust the margin

--- lib/include/elements/element/gallery/dialog.hpp
+++ lib/include/elements/element/gallery/dialog.hpp
@@ -45,7 +45,7 @@ namespace cycfi { namespace elements
          return share(
             key_intercept(align_center_middle(
                layer(
-                  margin({ 20, 20, 20, 20 },
+                  margin({ 32, 32, 32, 32 },
                      std::forward<Content>(content)
                   ),
                   panel{ /* opacity */0.98 }

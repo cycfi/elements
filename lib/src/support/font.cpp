563da681577d02dbf205ad60085433720ba95ca5
Joel de Guzman <djowel@gmail.com>
2020-08-24 14:14:39 +0800

    test ascii only

--- lib/src/support/font.cpp
+++ lib/src/support/font.cpp
@@ -47,14 +47,14 @@ namespace cycfi { namespace elements
       inline void ltrim(std::string& s)
       {
          s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            [](int ch) { return !std::isspace(ch) && ch != '"'; }
+            [](int ch) { return int(ch) > 0 && !std::isspace(ch) && ch != '"'; }
          ));
       }
 
       inline void rtrim(std::string& s)
       {
          s.erase(std::find_if(s.rbegin(), s.rend(),
-            [](int ch) { return !std::isspace(ch) && ch != '"'; }
+            [](int ch) { return int(ch) > 0 && !std::isspace(ch) && ch != '"'; }
          ).base(), s.end());
       }
 

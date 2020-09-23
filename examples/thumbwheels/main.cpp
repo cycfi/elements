2b004926be533839d20880bdd784fdd64be4eddd
djowel <djowel@gmail.com>
2020-09-23 18:19:13 +0800

    thumbwheel FTW!

--- examples/thumbwheels/main.cpp
+++ examples/thumbwheels/main.cpp
@@ -4,6 +4,7 @@
    Distributed under the MIT License (https://opensource.org/licenses/MIT)
 =============================================================================*/
 #include <elements.hpp>
+#include <sstream>
 
 using namespace cycfi::elements;
 
@@ -11,115 +12,63 @@ using namespace cycfi::elements;
 auto constexpr bkd_color = rgba(35, 35, 37, 255);
 auto background = box(bkd_color);
 
-// using slider_ptr = std::shared_ptr<basic_slider_base>;
-// slider_ptr vsliders[3];
-
-// using thumbwheel_ptr = std::shared_ptr<thumbwheel_base>;
-// thumbwheel_ptr thumbwheels[3];
-
-// template <bool is_vertical>
-// auto make_markers()
-// {
-//    auto track = basic_track<5, is_vertical>();
-//    return slider_labels<10>(
-//       slider_marks<40>(track),         // Track with marks
-//       0.8,                             // Label font size (relative size)
-//       "0", "1", "2", "3", "4",         // Labels
-//       "5", "6", "7", "8", "9", "10"
-//    );
-// }
-
-// auto make_vslider(int index)
-// {
-//    vsliders[index] = share(slider(
-//       basic_thumb<25>(),
-//       make_markers<true>(),
-//       (index + 1) * 0.25
-//    ));
-//    return align_center(vmargin({ 20, 20 }, hold(vsliders[index])));
-// }
-
-// auto make_vsliders()
-// {
-//    return hmin_size(300,
-//       htile(
-//          make_vslider(0),
-//          make_vslider(1),
-//          make_vslider(2)
-//       )
-//    );
-// }
-
-auto make_thumbwheel()
+template <typename E>
+auto decorate(E&& e)
 {
-   auto&& make_label =
+   return hsize(170, align_center(margin({ 25, 5, 25, 5 },
+      std::forward<E>(e)
+   )));
+}
+
+auto make_label(std::string text = "")
+{
+   return decorate(heading(text)
+      .font_color(get_theme().indicator_hilite_color)
+      .font_size(24)
+   );
+};
+
+auto make_thumbwheel1()
+{
+   auto&& compose =
       [](std::size_t index)
       {
          auto text = "Item no.  " + std::to_string(index+1);
-         return share(margin({ 25, 5, 25, 5 },
-            heading(text)
-               .font_color(get_theme().indicator_hilite_color)
-               .font_size(24)
-         ));
+         return share(make_label(text));
       };
 
-   auto thumbwheel = share(vthumbwheel(20, make_label));
+   auto tw = share(vthumbwheel(20, compose));
 
-   return align_center(
+   return top_margin(20,
       layer(
-         hold(thumbwheel),
+         hold(tw),
          frame{}
       )
    );
 }
 
-// auto make_thumbwheels()
-// {
-//    return hmargin(20,
-//       vtile(
-//          make_thumbwheel(0),
-//          make_thumbwheel(1),
-//          make_thumbwheel(2)
-//       )
-//    );
-// }
+auto make_thumbwheel2(char const* unit, float offset, float scale, int precision)
+{
+   auto label = make_label();
 
-// auto make_controls()
-// {
-//    return
-//       margin({ 20, 10, 20, 10 },
-//          vmin_size(400,
-//             htile(
-//                margin({ 20, 20, 20, 20 }, pane("Vertical Sliders", make_vsliders(), 0.8f)),
-//                hstretch(0.5, margin({ 20, 20, 20, 20 }, pane("Thumbwheels", make_thumbwheels(), 0.8f)))
-//             )
-//          )
-//       );
-// }
+   auto&& as_string =
+      [=](double val)
+      {
+         std::ostringstream out;
+         out.precision(precision);
+         out << std::fixed << ((val * scale) + offset) << unit;
+         return out.str();
+      };
 
-// void link_control(int index, view& view_)
-// {
-//    vsliders[index]->on_change =
-//       [index, &view_](double val)
-//       {
-//          thumbwheels[index]->thumbwheel_base::value(val);
-//          view_.refresh(*thumbwheels[index]);
-//       };
+   auto tw = share(thumbwheel(as_label<double>(as_string, label)));
 
-//    thumbwheels[index]->on_change =
-//       [index, &view_](double val)
-//       {
-//          vsliders[index]->slider_base::value(val);
-//          view_.refresh(*vsliders[index]);
-//       };
-// }
-
-// void link_controls(view& view_)
-// {
-//    link_control(0, view_);
-//    link_control(1, view_);
-//    link_control(2, view_);
-// }
+   return top_margin(20,
+      layer(
+         hold(tw),
+         frame{}
+      )
+   );
+}
 
 int main(int argc, char* argv[])
 {
@@ -131,7 +80,13 @@ int main(int argc, char* argv[])
 
    view_.content(
 
-      align_center_middle(make_thumbwheel()),
+      margin({ 20, 0, 20, 20 },
+         vtile(
+            align_center_middle(make_thumbwheel1()),
+            align_center_middle(make_thumbwheel2(" Hz", 20, 100, 2)),
+            align_center_middle(make_thumbwheel2(" ms", 0, 10000, 0))
+         )
+      ),
 
       // make_controls(),
       background
--- lib/include/elements/element/gallery/thumbwheel.hpp
+++ lib/include/elements/element/gallery/thumbwheel.hpp
@@ -8,70 +8,19 @@
 
 #include <elements/element/dial.hpp>
 #include <elements/element/dynamic_list.hpp>
-// #include <elements/element/align.hpp>
-// #include <elements/element/layer.hpp>
-// #include <elements/element/indirect.hpp>
-// #include <elements/element/tile.hpp>
-// #include <elements/element/gallery/caption.hpp>
 
 namespace cycfi { namespace elements
 {
    template <typename F>
-   inline auto vthumbwheel(double init_value, std::size_t num_items, F&& compose)
+   inline auto vthumbwheel(std::size_t num_items, F&& compose)
    {
       using ftype = remove_cvref_t<F>;
       auto body = basic_vthumbwheel(
          dynamic_list{ basic_cell_composer(num_items, std::forward<ftype>(compose)) }
        , 1.0 / (num_items-1)
       );
-      return thumbwheel(std::move(body), init_value);
+      return thumbwheel(std::move(body), 0.0);
    }
-
-   template <typename F>
-   inline auto vthumbwheel(std::size_t num_items, F&& compose)
-   {
-      using ftype = remove_cvref_t<F>;
-      return vthumbwheel(0.0, num_items, std::forward<ftype>(compose));
-   }
-
-   // ////////////////////////////////////////////////////////////////////////////
-   // // vthumbwheel
-   // ////////////////////////////////////////////////////////////////////////////
-   // template <typename FirstLabel, typename... Label>
-   // inline auto vthumbwheel(double init_value, FirstLabel&& first_label, Label&&... label)
-   // {
-   //    auto body = basic_vthumbwheel(
-   //       vtile(std::forward<FirstLabel>(first_label), std::forward<Label>(label)...)
-   //     , 1.0 / sizeof...(Label)
-   //    );
-   //    return thumbwheel(std::move(body), init_value);
-   // }
-
-   // template <typename... Label>
-   // inline auto vthumbwheel(Label&&... label)
-   // {
-   //   return vthumbwheel(0.0, std::forward<Label>(label)...);
-   // }
-
-   // ////////////////////////////////////////////////////////////////////////////
-   // // hthumbwheel
-   // ////////////////////////////////////////////////////////////////////////////
-   // template <typename FirstLabel, typename... Label>
-   // inline auto hthumbwheel(double init_value, FirstLabel&& first_label, Label&&... label)
-   // {
-   //    auto body = basic_hthumbwheel(
-   //       htile(std::forward<FirstLabel>(first_label), std::forward<Label>(label)...)
-   //     , 1.0 / sizeof...(Label)
-   //    );
-   //    return thumbwheel(std::move(body), init_value);
-   // }
-
-   // template <typename... Label>
-   // inline auto hthumbwheel(Label&&... label)
-   // {
-   //   return hthumbwheel(0.0, std::forward<Label>(label)...);
-   // }
-
 }}
 
 #endif

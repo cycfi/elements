9a7814501ad4c4b392a4adf0c242b06f1c8e7141
Johann <johannphilippe@lilo.org>
2022-01-06 16:46:48 +0100

    author info

--- examples/icons_list/main.cpp
+++ examples/icons_list/main.cpp
@@ -1,5 +1,5 @@
 /*=============================================================================
-   Copyright (c) 2021 Johann Philippe
+   Copyright (c) 2022 Johann Philippe
 
    Distributed under the MIT License (https://opensource.org/licenses/MIT)
 =============================================================================*/

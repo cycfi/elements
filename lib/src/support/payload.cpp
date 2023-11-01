51f659a925f00cc72ee46b287a29408f1f947abc
djowel <djowel@gmail.com>
2023-11-01 09:53:00 +0800

    Refinements

--- lib/src/support/payload.cpp
+++ lib/src/support/payload.cpp
@@ -7,6 +7,19 @@
 
 namespace cycfi::elements
 {
+   bool contains_filepaths(payload const& data)
+   {
+      // Make sure there's a "text/uri-list" MIME data type and that there's
+      // at least one valid file URI.
+      auto i = data.find("text/uri-list");
+      if (i != data.end())
+      {
+         std::string_view text = std::get<std::string>(i->second);
+         return text.find("file://") == 0;
+      }
+      return false;
+   }
+
    std::vector<fs::path> get_filepaths(payload const& data)
    {
       std::vector<fs::path> paths;
@@ -16,14 +29,21 @@ namespace cycfi::elements
          size_t pos = 0;
          size_t found;
 
+         auto fix_path = [&](std::string_view path) -> std::string_view
+         {
+            if (path.find("file://") == 0)
+               return path.substr(7);
+            return std::string_view{""};
+         };
+
          while ((found = text.find('\n', pos)) != std::string::npos)
          {
-            // Note: +7 skips the "file://" prefix
-            paths.push_back(text.substr(pos+7, found - pos));
+            if (auto path = fix_path(text.substr(pos, found - pos)); !path.empty())
+               paths.push_back(path);
             pos = found + 1;
          }
-         // Note: +7 skips the "file://" prefix
-         paths.push_back(text.substr(pos+7));
+         if (auto path = fix_path(text.substr(pos)); !path.empty())
+            paths.push_back(path);
       }
       return paths;
    }

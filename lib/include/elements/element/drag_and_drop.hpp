fab464c0c42d9f43dd18225166e8e9e228273da1
djowel <djowel@gmail.com>
2023-11-04 14:05:19 +0800

    WIP list inserter example

--- lib/include/elements/element/drag_and_drop.hpp
+++ lib/include/elements/element/drag_and_drop.hpp
@@ -12,33 +12,70 @@
 
 namespace cycfi { namespace elements
 {
-   class drop_box_base : public proxy_base
+   class drop_base : public proxy_base
    {
    public:
 
       using base_type = proxy_base;
-      using on_drop_function = std::function<bool(drop_info const& info)>;
+      using mime_types = std::set<std::string>;
 
-                              drop_box_base(std::initializer_list<std::string> mime_types_);
-      void                    draw(context const& ctx) override;
+                              drop_base(std::initializer_list<std::string> mime_types_);
       bool                    wants_control() const override;
-
       void                    track_drop(context const& ctx, drop_info const& info, cursor_tracking status) override;
       bool                    drop(context const& ctx, drop_info const& info) override;
 
-      on_drop_function        on_drop = [](drop_info const&){ return false; };
+      bool                    is_tracking() const     { return _is_tracking; }
+      mime_types const&       get_mime_types() const  { return _mime_types; }
 
    private:
-
-      using mime_types = std::set<std::string>;
 
       bool                    _is_tracking = false;
       mime_types              _mime_types;
    };
 
+   class drop_box_base : public drop_base
+   {
+   public:
+
+      using base_type = drop_base;
+      using on_drop_function = std::function<bool(drop_info const& info)>;
+
+                              drop_box_base(std::initializer_list<std::string> mime_types_);
+      void                    draw(context const& ctx) override;
+      bool                    drop(context const& ctx, drop_info const& info) override;
+
+      on_drop_function        on_drop = [](drop_info const& /*info*/){ return false; };
+   };
+
    template <typename Subject>
    inline proxy<Subject, drop_box_base>
    drop_box(Subject&& subject, std::initializer_list<std::string> mime_types)
+   {
+      return {std::forward<Subject>(subject), mime_types};
+   }
+
+   class drop_inserter_base : public drop_base
+   {
+   public:
+
+      using base_type = drop_base;
+      using on_drop_function = std::function<bool(drop_info const& info, std::size_t ix)>;
+
+                              drop_inserter_base(std::initializer_list<std::string> mime_types_);
+      void                    draw(context const& ctx) override;
+      void                    track_drop(context const& ctx, drop_info const& info, cursor_tracking status) override;
+      bool                    drop(context const& ctx, drop_info const& info) override;
+
+      on_drop_function        on_drop = [](drop_info const& /*info*/, std::size_t /*ix*/){ return false; };
+
+   public:
+
+      int                     _insertion_pos = -1;
+   };
+
+   template <typename Subject>
+   inline proxy<Subject, drop_inserter_base>
+   drop_inserter(Subject&& subject, std::initializer_list<std::string> mime_types)
    {
       return {std::forward<Subject>(subject), mime_types};
    }

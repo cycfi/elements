dda607bb0d1e10378581c0bc987b088f646f444b
Joel de Guzman <djowel@gmail.com>
2024-04-02 19:39:29 +0800

    Add the outer directory to the search path for resources

--- lib/host/windows/base_view.cpp
+++ lib/host/windows/base_view.cpp
@@ -505,8 +505,8 @@ namespace cycfi { namespace elements
                MessageBoxW(nullptr, L"Could not register class", L"Error", MB_OK);
 
             auto pwd = fs::current_path();
-            auto resource_path = pwd / "resources";
-            add_search_path(resource_path);
+            add_search_path(pwd / "resources");
+            add_search_path(pwd.parent_path() / "resources");
          }
       };
    }

239a3b92a16af7b668f0e1b8062ce783b9528124
djowel <djowel@gmail.com>
2024-04-13 04:12:07 +0800

    Revert "When adding a layer, don't automatically make it the focus"
    This reverts commit 88cebaabc75042da922608e5deff04b216c38267.

--- lib/include/elements/view.hpp
+++ lib/include/elements/view.hpp
@@ -244,8 +244,17 @@ namespace cycfi { namespace elements
          io().post(
             [e, this]
             {
+               auto wants_focus = e->wants_focus();
+               if (wants_focus)
+                  end_focus();
                _content.push_back(e);
                layout(*e);
+               if (e->wants_focus())
+               {
+                  _is_focus = true;
+                  begin_focus();
+                  _is_focus = _main_element.focus();
+               }
             }
          );
       }
@@ -265,9 +274,16 @@ namespace cycfi { namespace elements
                auto i = std::find(_content.begin(), _content.end(), e);
                if (i != _content.end())
                {
+                  if (e->wants_focus())
+                  {
+                     end_focus();
+                     refresh(*e);
+                  }
                   _content.erase(i);
                   _content.reset();
+                  _content.begin_focus(element::focus_request::restore_previous);
                   layout();
+                  _is_focus = _main_element.focus();
                }
             }
          );

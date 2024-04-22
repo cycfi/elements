ac959c48bb84b0549d2c5c3d5dfca480bb70ee79
djowel <djowel@gmail.com>
2024-04-22 16:23:19 +0800

    Remove gitsubmodule check. It's more trouble than its worth (reported problems with Windows)

--- cmake/GitSubmoduleCheck.cmake
+++
@@ -1,9 +0,0 @@
-# https://gist.github.com/scivision/bb1d47a9529e153617414e91ff5390af
-find_package(Git REQUIRED)
-function(git_submodule_check dir)
-   if(NOT EXISTS "${dir}/CMakeLists.txt")
-      execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
-         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-         COMMAND_ERROR_IS_FATAL ANY)
-   endif()
-endfunction()

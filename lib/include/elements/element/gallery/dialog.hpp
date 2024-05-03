06794478671ddc214567caca311d18a392f415a8
djowel <djowel@gmail.com>
2024-05-04 06:15:21 +0800

    Remove delay when processing a button click. It causes problems when the popup is expected to be closed after the click.

--- lib/include/elements/element/gallery/dialog.hpp
+++ lib/include/elements/element/gallery/dialog.hpp
@@ -117,13 +117,11 @@ namespace cycfi { namespace elements
       template <typename PopupPtr, typename ButtonPtr, typename F>
       void link_button(view& view_, PopupPtr popup, ButtonPtr btn, F&& f)
       {
-         using namespace std::chrono_literals;
-
          btn->on_click =
             [&view_, eptr = get(popup), f, btn = get(btn)](bool)
             {
                f();
-               view_.post(100ms,
+               view_.post(
                   [&view_, btn, eptr]()
                   {
                      if (auto b = btn.lock())

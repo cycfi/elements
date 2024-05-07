6c4a49f45c621b6825fdd4842606b95a12f8eea0
djowel <djowel@gmail.com>
2024-05-08 06:32:13 +0800

    Fixes #399 (In text boxes, on key-press, the caret should be visible without any delay)

--- lib/src/element/text.cpp
+++ lib/src/element/text.cpp
@@ -371,6 +371,7 @@ namespace cycfi { namespace elements
          )
          return false;
 
+      _show_caret = true;
       bool move_caret = false;
       bool save_x = false;
       bool handled = false;
@@ -882,6 +883,7 @@ namespace cycfi { namespace elements
    void basic_text_box::begin_focus(focus_request /*req*/)
    {
       _is_focus = true;
+      _show_caret = true;
       if (_select_start == -1)
          _select_start = _select_end = 0;
       scroll_into_view();

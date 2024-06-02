f9951c175012de242abb7c42f29a592e73f45759
djowel <djowel@gmail.com>
2024-06-02 22:17:02 +0800

    Documenting button

--- lib/include/elements/support/receiver.hpp
+++ lib/include/elements/support/receiver.hpp
@@ -14,8 +14,25 @@ namespace cycfi::elements
    class element;
    class view;
 
+   /**
+    * \brief
+    *    An abstract base class to provide editing functionality.
+    *
+    *    `receiver_base` defines the core editing functionalities: starting
+    *    an edit operation, doing an edit operation, and ending an edit
+    *    operation.
+    */
    struct receiver_base
    {
+      /**
+       * \enum receiver_base::editing
+       * \brief Represents the states of an editing operation.
+       *
+       * \value none No ongoing editing operation.
+       * \value begin_editing The beginning of an editing operation.
+       * \value while_editing Ongoing editing operation.
+       * \value end_editing End of an editing operation.
+       */
       enum editing { none, begin_editing, while_editing, end_editing };
 
       virtual              ~receiver_base() = default;
@@ -24,6 +41,12 @@ namespace cycfi::elements
       void                 end_edit(view& view_);
    };
 
+   /**
+    *  \struct receiver<T>
+    *  \brief An abstract base class to provide value control for a type `T`.
+    *
+    *  \tparam T The type of value that the receiver will handle.
+    */
    template <typename T>
    struct receiver : receiver_base
    {
@@ -36,6 +59,10 @@ namespace cycfi::elements
       virtual void         edit(view& view_, param_type val);
    };
 
+   /**
+    *  \struct receiver<std::string>
+    *  \brief Specialization of `receiver<T>` for `std::string`.
+    */
    template <>
    struct receiver<std::string> : receiver_base
    {
@@ -43,11 +70,17 @@ namespace cycfi::elements
       using getter_type = std::string const&;
       using param_type = string_view;
 
-      virtual void         value(param_type str) = 0;
+      virtual void         value(param_type val) = 0;
       virtual getter_type  value() const = 0;
       virtual void         edit(view& view_, param_type val);
    };
 
+   /**
+    *  \struct basic_receiver<T>
+    *  \brief Basic value receiver that saves the value in a member variable.
+    *
+    *  \tparam T The type of value that this receiver will handle.
+    */
    template <typename T>
    struct basic_receiver : receiver<T>
    {
@@ -55,8 +88,8 @@ namespace cycfi::elements
       using getter_type = typename receiver<T>::getter_type;
       using param_type = typename receiver<T>::param_type;
 
-      void                 value(param_type val) override { _val = val; }
-      T                    value() const override { return _val; }
+      void                 value(param_type val) override;
+      T                    value() const override;
 
    private:
 
@@ -66,6 +99,44 @@ namespace cycfi::elements
    ////////////////////////////////////////////////////////////////////////////
    // Inlines
    ////////////////////////////////////////////////////////////////////////////
+   namespace inlines {}
+
+   /**
+    * \brief Set the receiver's value.
+    * \param val The new value for the receiver.
+    */
+   template <typename T>
+   inline void basic_receiver<T>::value(param_type val)
+   {
+      _val = val;
+   }
+
+   /**
+    * \brief get the receiver's value.
+    * \return The value of the receiver.
+    */
+   template <typename T>
+   inline T basic_receiver<T>::value() const
+   {
+      return _val;
+   }
+
+   /**
+    * \brief
+    *    An edit function for setting the new value of type `T` and notifying
+    *    the view of the changes.
+    *
+    *    This function sets the new value using the provided `value()`
+    *    function and then notifies the view `view_` that the edit operation
+    *    has been performed via `notify_edit()` function of `receiver_base`.
+    *
+    * \tparam T
+    *    The type of value that the receiver manages.
+    * \param view_
+    *    A reference to a `view` object.
+    * \param val
+    *    The new value of the receiver of type `T`.
+    */
    template <typename T>
    inline void receiver<T>::edit(view& view_, param_type val)
    {
@@ -73,6 +144,15 @@ namespace cycfi::elements
       receiver_base::notify_edit(view_);
    }
 
+   /**
+    * \brief
+    *    An edit function specialized for `std::string` type.
+    *
+    * \param view_
+    *    A reference to a `view` object.
+    * \param val
+    *    The new value of the receiver of type `std::string`.
+    */
    inline void receiver<std::string>::edit(view& view_, param_type val)
    {
       value(val);

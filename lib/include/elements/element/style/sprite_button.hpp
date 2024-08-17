dd03b64a1575ffb10896b1ca48c668ae688a13dd
djowel <djowel@gmail.com>
2024-08-17 18:25:29 +0800

    Clean up and documentation

--- lib/include/elements/element/style/sprite_button.hpp
+++ lib/include/elements/element/style/sprite_button.hpp
@@ -11,6 +11,19 @@
 
 namespace cycfi::elements
 {
+   /**
+    * \class
+    *    sprite_button_styler
+    *
+    * \brief
+    *    A class template that styles a button with a sprite.
+    *
+    *    This class template inherits from a base button class and provides
+    *    additional functionality to style the button using a sprite. The
+    *    sprite can be used to represent different states of the button.
+    *
+    * \tparam Base The base button class to inherit from.
+    */
    template <typename Base>
    class sprite_button_styler : public Base
    {
@@ -46,6 +59,20 @@ namespace cycfi::elements
    // Inlines
    //--------------------------------------------------------------------------
 
+   /**
+    * \brief
+    *    Draws the sprite button with the given context.
+    *
+    *    This function overrides the draw function in the base class to
+    *    provide specific drawing functionality for the sprite button. It
+    *    determines the appropriate frame of the sprite to display based on
+    *    the button's state (enabled/disabled, value, and highlight).
+    *
+    * \param ctx
+    *    The context within which the element should be drawn. The context
+    *    provides information about the view, canvas, and other properties
+    *    that might be necessary for drawing the element.
+    */
    template <typename Base>
    inline void sprite_button_styler<Base>::draw(context const& ctx)
    {
@@ -61,6 +88,30 @@ namespace cycfi::elements
       }
    }
 
+   /**
+    * \brief
+    *    Creates a momentary button with a sprite button styler.
+    *
+    *    This function template creates a proxy object for a momentary button
+    *    styled with a sprite. A momentary button is a button that only
+    *    remains active while it is being pressed.
+    *
+    * \tparam Base
+    *    The base type of the momentary button. `Base` should be a momentary
+    *    button that adheres to ` MomentaryButton` concept with
+    *    `basic_button` as the default type.
+    *
+    * \tparam Styler
+    *    The type of the sprite styler. The sprite styler must adhere to the
+    *    SpriteSubject concept,
+    *
+    * \param s
+    *    The sprite styler to be used for the button.
+    *
+    * \return
+    *    A proxy object that combines the sprite button styler with the
+    *    momentary button.
+    */
    template <
       concepts::MomentaryButton Base
     , concepts::SpriteSubject Styler
@@ -71,6 +122,30 @@ namespace cycfi::elements
       return {std::forward<Styler>(s)};
    }
 
+   /**
+    * \brief
+    *    Creates a toggle button with a sprite button styler.
+    *
+    *    This function template creates a proxy object for a toggle button
+    *    styled with a sprite. A toggle button is a button that remains
+    *    active until it is pressed again.
+    *
+    * \tparam Base
+    *    The base type of the latching button. `Base` should be a toggle
+    *    button that adheres to ` ToggleButton` concept with
+    *    `basic_toggle_button` as the default type.
+    *
+    * \tparam Styler
+    *    The type of the sprite styler. The sprite styler must adhere to the
+    *    SpriteSubject concept,
+    *
+    * \param s
+    *    The sprite styler to be used for the button.
+    *
+    * \return
+    *    A proxy object that combines the sprite button styler with the
+    *    toggle button.
+    */
    template <
       concepts::ToggleButton Base
     , concepts::SpriteSubject Styler
@@ -81,6 +156,31 @@ namespace cycfi::elements
       return {std::forward<Styler>(s)};
    }
 
+   /**
+    * \brief
+    *    Creates a latching button with a sprite button styler.
+    *
+    *    This function template creates a proxy object for a latching button
+    *    styled with a sprite. A latching button is a button that maintains
+    *    its state after being clicked and holds this state indefinitely
+    *    until it is reset programmatically.
+    *
+    * \tparam Base
+    *    The base type of the latching button. `Base` should be a latching
+    *    button that adheres to `LatchingButton` concept with
+    *    `basic_latching_button` as the default type.
+    *
+    * \tparam Styler
+    *    The type of the sprite styler. The sprite styler must adhere to the
+    *    SpriteSubject concept,
+    *
+    * \param s
+    *    The sprite styler to be used for the button.
+    *
+    * \return
+    *    A proxy object that combines the sprite button styler with the
+    *    latching button.
+    */
    template <
       concepts::LatchingButton Base
     , concepts::SpriteSubject Styler
--- lib/include/elements/element/style/sprite_dial.hpp
+++ lib/include/elements/element/style/sprite_dial.hpp
@@ -12,6 +12,22 @@
 
 namespace cycfi::elements
 {
+   /**
+    * \class
+    *    sprite_dial_styler
+    *
+    * \brief
+    *    A class template that styles a dial control with a sprite.
+    *
+    *    This class template inherits from a base dial class and provides
+    *    additional functionality to style the dial using a sprite. The
+    *    sprite can be used to represent different states or positions of the
+    *    dial. The dial has a value that is a normalized `double` in the
+    *    range [0, 1]. This value maps to different frames in the sprite to
+    *    visually show the dial position.
+    *
+    * \tparam Base The base dial class to inherit from.
+    */
    template <typename Base>
    class sprite_dial_styler : public Base
    {
@@ -30,6 +46,20 @@ namespace cycfi::elements
    // Inlines
    //--------------------------------------------------------------------------
 
+   /**
+    * \brief
+    *    Draws the sprite dial with the given context.
+    *
+    *    The draw function determines the appropriate frame of the sprite to
+    *    display based on the dial's value: a normalized `double` in the
+    *    range [0, 1], which maps to different frames in the sprite to
+    *    visually show the dial position.
+    *
+    * \param ctx
+    *    The context within which the element should be drawn. The context
+    *    provides information about the view, canvas, and other properties
+    *    that might be necessary for drawing the element.
+    */
    template <typename Base>
    inline void sprite_dial_styler<Base>::draw(context const& ctx)
    {
@@ -40,6 +70,25 @@ namespace cycfi::elements
       }
    }
 
+   /**
+    * \brief
+    *    Creates a dial control with a sprite dial styler.
+    *
+    * \tparam Styler
+    *    The type of the sprite styler. The sprite styler must adhere to the
+    *    SpriteSubject concept.
+    *
+    * \param styler
+    *    The sprite styler to be used for the dial.
+    *
+    * \param init_value
+    *    The initial value of the dial, which is a normalized `double` in the
+    *    range [0, 1].
+    *
+    * \return
+    *    A proxy object that combines the sprite dial styler with the basic
+    *    dial.
+    */
    template <concepts::SpriteSubject Styler>
    inline proxy<remove_cvref_t<Styler>, sprite_dial_styler<basic_dial>>
    dial(Styler&& styler, double init_value)

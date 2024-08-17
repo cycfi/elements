70a257f10c4d9007d34a1a9ea38d49e93bd2f844
djowel <djowel@gmail.com>
2024-08-17 18:02:25 +0800

    Use base injection instead of proxy

--- lib/include/elements/element/style/sprite_button.hpp
+++ lib/include/elements/element/style/sprite_button.hpp
@@ -11,14 +11,12 @@
 
 namespace cycfi::elements
 {
-   template <typename Subject>
-   class sprite_button_styler : public proxy<Subject>
+   template <typename Base>
+   class sprite_button_styler : public Base
    {
    public:
 
-      using base_type = proxy<Subject>;
-
-                              sprite_button_styler(Subject subject);
+      using Base::Base;
 
       void                    draw(context const& ctx) override;
    };
@@ -27,62 +25,70 @@ namespace cycfi::elements
       concepts::MomentaryButton Base = basic_button
     , concepts::SpriteSubject Styler
    >
-   inline proxy<sprite_button_styler<remove_cvref_t<Styler>>, Base>
-   momentary_button(Styler&& s)
-   {
-      using styler_type = sprite_button_styler<remove_cvref_t<Styler>>;
-      return {styler_type{std::forward<Styler>(s)}};
-   }
+   inline proxy<remove_cvref_t<Styler>, sprite_button_styler<Base>>
+   momentary_button(Styler&& s);
 
    template <
       concepts::ToggleButton Base = basic_toggle_button
     , concepts::SpriteSubject Styler
    >
-   inline proxy<sprite_button_styler<remove_cvref_t<Styler>>, Base>
-   toggle_button(Styler&& s)
-   {
-      using styler_type = sprite_button_styler<remove_cvref_t<Styler>>;
-      return {styler_type{std::forward<Styler>(s)}};
-   }
+   inline proxy<remove_cvref_t<Styler>, sprite_button_styler<Base>>
+   toggle_button(Styler&& s);
 
    template <
       concepts::LatchingButton Base = basic_latching_button
     , concepts::SpriteSubject Styler
    >
-   inline proxy<sprite_button_styler<remove_cvref_t<Styler>>, Base>
-   latching_button(Styler&& s)
-   {
-      using styler_type = sprite_button_styler<remove_cvref_t<Styler>>;
-      return {styler_type{std::forward<Styler>(s)}};
-   }
+   inline proxy<remove_cvref_t<Styler>, sprite_button_styler<Base>>
+   latching_button(Styler&& s);
 
    //--------------------------------------------------------------------------
    // Inlines
    //--------------------------------------------------------------------------
 
-   template <typename Subject>
-   inline sprite_button_styler<Subject>::sprite_button_styler(Subject subject)
-    : base_type{std::move(subject)}
-   {}
-
-   template <typename Subject>
-   inline void sprite_button_styler<Subject>::draw(context const& ctx)
+   template <typename Base>
+   inline void sprite_button_styler<Base>::draw(context const& ctx)
    {
-      auto btn = find_parent<basic_button*>(ctx);
-      if (!btn)
-         return;
-
-      auto sp = find_subject<sprite*>(this);
-      if (sp)
+      if (auto sp = find_subject<sprite*>(this))
       {
-         auto value = btn->value();
-         auto hilite = btn->hilite();
+         auto value = this->value();
+         auto hilite = this->hilite();
          if (!ctx.enabled && sp->num_frames() > 4)
             sp->index(4); // disabled
          else
             sp->index((value? 2 : 0) + hilite); // enabled
-         sp->draw(ctx);
+         Base::draw(ctx);
       }
+   }
+
+   template <
+      concepts::MomentaryButton Base
+    , concepts::SpriteSubject Styler
+   >
+   inline proxy<remove_cvref_t<Styler>, sprite_button_styler<Base>>
+   momentary_button(Styler&& s)
+   {
+      return {std::forward<Styler>(s)};
+   }
+
+   template <
+      concepts::ToggleButton Base
+    , concepts::SpriteSubject Styler
+   >
+   inline proxy<remove_cvref_t<Styler>, sprite_button_styler<Base>>
+   toggle_button(Styler&& s)
+   {
+      return {std::forward<Styler>(s)};
+   }
+
+   template <
+      concepts::LatchingButton Base
+    , concepts::SpriteSubject Styler
+   >
+   inline proxy<remove_cvref_t<Styler>, sprite_button_styler<Base>>
+   latching_button(Styler&& s)
+   {
+      return {std::forward<Styler>(s)};
    }
 }
 
--- lib/include/elements/element/style/sprite_dial.hpp
+++ lib/include/elements/element/style/sprite_dial.hpp
@@ -12,52 +12,39 @@
 
 namespace cycfi::elements
 {
-   template <typename Subject>
-   class sprite_dial_styler : public proxy<Subject>
+   template <typename Base>
+   class sprite_dial_styler : public Base
    {
    public:
 
-      using base_type = proxy<Subject>;
-
-                              sprite_dial_styler(Subject subject);
+      using Base::Base;
 
       void                    draw(context const& ctx) override;
    };
 
    template <concepts::SpriteSubject Styler>
-   inline proxy<sprite_dial_styler<remove_cvref_t<Styler>>, basic_dial>
+   inline proxy<remove_cvref_t<Styler>, sprite_dial_styler<basic_dial>>
    dial(Styler&& styler, double init_value = 0.0);
 
    //--------------------------------------------------------------------------
    // Inlines
    //--------------------------------------------------------------------------
 
-   template <typename Subject>
-   inline sprite_dial_styler<Subject>::sprite_dial_styler(Subject subject)
-    : base_type{std::move(subject)}
-   {}
-
-   template <typename Subject>
-   inline void sprite_dial_styler<Subject>::draw(context const& ctx)
+   template <typename Base>
+   inline void sprite_dial_styler<Base>::draw(context const& ctx)
    {
-      auto dial = find_parent<basic_dial*>(ctx);
-      if (!dial)
-         return;
-
-      auto sp = find_subject<sprite*>(this);
-      if (sp)
+      if (auto sp = find_subject<sprite*>(this))
       {
-         sp->index(dial->value() * (sp->num_frames()-1));
-         sp->draw(ctx);
+         sp->index(this->value() * (sp->num_frames()-1));
+         Base::draw(ctx);
       }
    }
 
    template <concepts::SpriteSubject Styler>
-   inline proxy<sprite_dial_styler<remove_cvref_t<Styler>>, basic_dial>
+   inline proxy<remove_cvref_t<Styler>, sprite_dial_styler<basic_dial>>
    dial(Styler&& styler, double init_value)
    {
-      using styler_type = sprite_dial_styler<remove_cvref_t<Styler>>;
-      return {styler_type{std::forward<Styler>(styler)}, init_value};
+      return {std::forward<Styler>(styler), init_value};
    }
 }
 
